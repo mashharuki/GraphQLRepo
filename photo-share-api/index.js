7a3ac49e78e41559947abd7205e16ca70333db0b
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-08-02 22:16:40 +0900

    5.2章まで完了

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -1,10 +1,13 @@
 // const express = require('express')
 const { ApolloServer } = require('apollo-server')
 const { readFileSync } = require('fs');
+const { GraphQLScalarType } = require('graphql')
 
 // read GraphQL config file
 // var typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
 var typeDefs = `
+      scalar DateTime
+
       # define Photo Type
       type Photo {
             id: ID!
@@ -13,6 +16,8 @@ var typeDefs = `
             description: String
             category: PhotoCategory!
             postedBy: User!
+            taggedUsers: [User!]!
+            created: DateTime!
       }
 
       type User {
@@ -20,6 +25,7 @@ var typeDefs = `
             name: String
             avatar: String
             postedPhotos: [Photo!]!
+            inPhotos: [Photo!]! 
       }
 
       # PhotoCategory
@@ -63,22 +69,34 @@ var photos = [
             "name": "Dropping the Heart Chute",
             "description": "The heart chute is one of my favorite chutes",
             "category": "ACTION",
-            "githubUser": "gPlake"
+            "githubUser": "gPlake",
+            "created": "2022:08:02"
       },
       {
             "id": "2",
             "name": "Enjoying the sunshine",
             "category": "SELFIE",
-            "githubUser": "sSchmidt"
+            "githubUser": "sSchmidt",
+            "created": "2022:08:02"
       },
       {
             "id": "3",
             "name": "Gunbarrel 25",
             "description": "25 laps on gunbarrel today",
             "category": "LANDSCAPE",
-            "githubUser": "sSchmidt"
+            "githubUser": "sSchmidt",
+            "created": "2022:08:02"
       }
 ];
+// Array of tag
+var tags = [
+      { "photoID": "1", "userID": "gPlake" },
+      { "photoID": "2", "userID": "sSchmidt" },
+      { "photoID": "2", "userID": "mHattrup" },
+      { "photoID": "2", "userID": "gPlake" },
+];
+// data
+var d = new Date(`4/18/2018`)
 
 // Revolver
 const resolvers = {
@@ -93,7 +111,8 @@ const resolvers = {
                   // create new photo
                   var newPhoto = {
                         id: _id++,
-                        ...args.input
+                        ...args.input,
+                        created: new Date()
                   };
                   // add
                   photos.push(newPhoto)
@@ -104,13 +123,29 @@ const resolvers = {
             url: parent => `http://yoursite.com/img${parent.id}.jpg`,
             postedBy: parent => {
                   return users.find(u => u.githubLogin === parent.githubLogin)
-            }
+            },
+            taggedUsers : parent => tags
+                                          .filter(tag => tag.photoID === parent.id)
+                                          .map(tag => tag.userID)
+                                          .map(userID => users.find(u => u.githubLogin === userID)),
+            
       },
       User: {
             postedPhotos: parent => {
                   return photos.filter(p => p.githubLogin === parent.githubLogin)
-            }
-      }
+            },
+            inPhotos: parent => tags
+                  .filter(tag => tag.userID === parent.id)
+                  .map(tag => tag.photoID)
+                  .map(photoID => photos.find(p => p.id === photoID))
+      },
+      DateTime: new GraphQLScalarType({
+            name: `DateTime`,
+            description: "A valid data time value",
+            parseValue: value => new Date(value),
+            serialize: value => new Date(value).toISOString(),
+            parseLiteral: ast => ast.value
+      }),
 }
 
 // create server instance
