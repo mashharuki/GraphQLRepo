219d18b0f38eb8e9c5d2838695e44204703dcfe5
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-08-06 15:57:45 +0900

    クライアントサイドからGraphqlを使うための設定を完了

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,22 @@
+import { ApolloProvider } from "@apollo/client";
+import ApolloClient from "apollo-boost";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+// create client instance
+const client = new ApolloClient({ 
+  uri: 'http://localhost:4000/graphql', 
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
